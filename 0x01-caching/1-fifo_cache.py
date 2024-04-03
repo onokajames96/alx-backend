d5eacd3a1ff8dabae8b4300b6c9ad854d9f0be69
onokajames96 <jamesonoka996@gmail.com>
2024-04-03 21:22:13 +0000

    fiffo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+Class module
 """
 from base_caching import BaseCaching
 
@@ -7,9 +8,18 @@ from base_caching import BaseCaching
 class FIFOCache(BaseCaching):
     """Class FIFOcache"""
     def __init__(self):
+        """Initialization"""
         super().__init__()
         self.queue = []
 
     def put(self, key, item):
-         if key is None or item is None:
+        """Assign to dict"""
+        if key is None or item is None:
             return
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """Gets data"""
+        return self.cache_data.get(key, None)
