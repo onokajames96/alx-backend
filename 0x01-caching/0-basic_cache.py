2a524aac81d237d17cbf9d73480bed7211d3f678
onokajames96 <jamesonoka996@gmail.com>
2024-04-03 18:02:52 +0000

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+"""
+Caching module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
 
+
+class BasicCache(BaseCaching):
+    """Class module"""
+
+    def put(self, key, item):
+        """Adding an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key.
+        """
+        return self.cache_data.get(key, None)
