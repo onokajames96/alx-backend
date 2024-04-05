932086302bb2845e1f11d1154b59befccd3cc359
onokajames96 <jamesonoka996@gmail.com>
2024-04-05 20:27:45 +0000

    caching taks

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
+"""LIFOCache"""
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class LIFOCache(BaseCaching):
+    """inherits from BaseCaching"""
     def __init__(self):
         super().__init__()
+        self.last_item_key = None
 
     def put(self, key, item):
+        """ add an item to cache"""
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = self.last_key
+            print("DISCARD:", discarded_key)
+            del self.cache_data[discarded_key]
+
+        self.last_key = key
 
     def get(self, key):
+        """ Get an item by ket"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
