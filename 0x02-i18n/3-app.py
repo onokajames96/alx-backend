f59a6680767a5074162f6adebd2c2aae25fe8658
onokajames96 <jamesonoka996@gmail.com>
2024-04-10 09:04:43 +0000

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" flask """
+from flask import Flask, render_template
+from flask import request
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Configuration
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Gets locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Rendering template"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
