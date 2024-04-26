c7272f2d0a029d920e41915824dd679abc047882
onokajames96 <jamesonoka996@gmail.com>
2024-04-26 02:28:54 +0000

    chahe

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,47 +1,46 @@
 #!/usr/bin/python3
-""" Caching MRU"""
+"""module  Caching MRU"""
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ Class module """
+    """Class implementing a Most Recently Used (MRU) cache."""
 
     def __init__(self):
+        """Initialize the MRU cache"""
         super().__init__()
         self.head, self.tail = 'head', 'tail'
         self.next, self.prev = {}, {}
         self.handle(self.head, self.tail)
 
     def handle(self, head, tail):
-        """handle elements """
+        """Initialize the linked list"""
         self.next[head], self.prev[tail] = tail, head
 
     def _remove(self, key):
-        """remove element """
+        """Remove an element from the cache."""
         self.handle(self.prev[key], self.next[key])
         del self.prev[key], self.next[key], self.cache_data[key]
 
     def _add(self, key, item):
-        """add element """
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+        """Add an element to the cache"""
+        if len(self.cache_data) > self.MAX_ITEMS - 1:
             self._remove(self.prev[self.tail])
         self.cache_data[key] = item
         self.handle(self.prev[self.tail], key)
         self.handle(key, self.tail)
 
     def put(self, key, item):
-        """Assings"""
+        """Add or update an item in the MRU cache."""
         if key and item:
             if key in self.cache_data:
                 self._remove(key)
             self._add(key, item)
 
     def get(self, key):
-        """ gets the data """
-        if key is None or self.cache_data.get(key) is None:
-            return None
+        """Retrieve an item from the MRU cache."""
         if key in self.cache_data:
-            value = self.cache_data[key]
-            self._remove(key)
-            self._add(key, value)
-            return value
+            self.handle(self.prev[key], self.next[key])
+            self.handle(self.prev[self.tail], key)
+            return self.cache_data[key]
+        return None
